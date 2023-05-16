5276bfb15a7925cdb12e7efb44dcf113cd70931f
danielvec <vechioladaniel@gmail.com>
2023-05-15 20:16:34 -0500

    Build a simple StaticPagesController to display a home page with a simple form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,13 @@
 class StaticPagesController < ApplicationController
+  def index
+    flickr = Flickr.new "9e85098b173e5ceef56440dce5397b3f", "f34789e4fb6b6bbe"
+    if !params[:username].nil?
+      @username = params[:username]
+      user   = flickr.people.findByUsername :username => @username
+
+      user_id     = user.id
+      @photos   = flickr.people.getPhotos :user_id => user_id
+      @photo_count = @photos.length
+    end
+  end
 end
